2b214fe9b4478abae8319e2d6b95af07d6fb3e3f
Vadym Yankovskyi <yankkv75@gmail.com>
2022-10-25 08:49:37 +0300

    Creating Blog section

---
+++ frontend/src/components/HomeScreen/BlogSection.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import '../../static/css/HomeScreen/Section.css'
+import { Link } from 'react-router-dom';
+
+
+function BlogSection() {
+    return (
+        <div className='section-container' id='about'>
+            <div className='section-wrapper'>
+                <div className='section-row'>
+                    <div className='column1'>
+                        <div className='text-wrapper'>
+                            <p className='top-line'>Blog</p>
+                            <h1 className='heading'>Latest News</h1>
+                            <div className='subtitle'>Read last news</div>
+                            <button className='btn-wrapper-section'>
+                                <Link to='/section' className='btn-link-section'>Read blog</Link>
+                            </button>
+                        </div>
+                    </div>
+                    <div className='column2'>
+                        <div className='img-wrap-section'>
+                            <img src='images/blog.jpg' className='section-img' alt='section' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default BlogSection
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import HeroSection from '../components/HomeScreen/HeroSection'
+import BlogSection from '../components/HomeScreen/BlogSection'
 
 function HomeScreen() {
     return (
         <>
             <HeroSection />
+            <BlogSection />
         </>
     )
 }

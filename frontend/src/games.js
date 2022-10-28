c845cf001441fe3ff6c91191b95ab57a65bcc844
Vadym Yankovskyi <yankkv75@gmail.com>
2022-10-28 22:34:50 +0300

    Creating a temporary file for games (dummy data)

---
+++ frontend/src/games.js
@@ -0,0 +1,58 @@
+const games = [
+    {
+        '_id': '1',
+        'image': '/images/images/cyberpunk2077.webp',
+        'name': 'Cyberpunk 2077',
+        'description': 'Cyberpunk 2077 is an open-world, action-adventure RPG set in the megalopolis of Night City, where you play as a cyberpunk mercenary wrapped-up in a do-or-die fight for survival. Explore the dark future, now upgraded with next-gen in mind and featuring free additional content!',
+        'trailer': 'https://www.youtube.com/watch?v=8X2kIfS6fb8',
+        'count_in_stock': 15,
+        'price':'25'
+        
+    },
+
+    {
+        '_id': '2',
+        'image': '/images/images/daysgone.webp',
+        'name': 'Days Gone',
+        'description': 'Days Gone is a massive open-world action-adventure game set in a harsh wilderness two years after a devastating global pandemic. Play as Deacon St. John, a drifter and bounty hunter who rides the broken road, fighting to survive while searching for a reason to live.',
+        'trailer': 'https://www.youtube.com/watch?v=FKtaOY9lMvM',
+        'count_in_stock': 15,
+        'price': '20'
+    },
+
+    {
+        '_id': '3',
+        'image': '/images/images/detroit.webp',
+        'name': 'Detroit Become Human',
+        'description': 'Detroit: Become Human is an adventure game played from a third-person view, which is subject to a set and controllable perspective. There are multiple playable characters who can die as the story continues without them; as a result, there is no "game over" message following a characters death.',
+        'trailer': 'https://www.youtube.com/watch?v=8a-EObAhYrg',
+        'count_in_stock': 15,
+        'price': '20'
+    },
+
+    {
+        '_id': '4',
+        'image': '/images/images/godofwar.webp',
+        'name': 'God Of War',
+        'description': 'In God of War, players control Kratos, a Spartan warrior who is sent by the Greek gods to kill Ares, the god of war. As the story progresses, Kratos is revealed to be Ares former servant, who had been tricked into killing his own family and is haunted by terrible nightmares.',
+        'trailer': 'https://www.youtube.com/watch?v=K0u_kAWLJOA',
+        'count_in_stock': 15,
+        'price': '25'
+    },
+
+    {
+        '_id': '5',
+        'image': '/images/images/rainbow.webp',
+        'name': 'Rainbow Six Siege',
+        'description': 'Rainbow Six Siege is a high-precision, tactical shooter that prioritises careful planning teamwork and finely tuned tactical play. Since its release in 2015, the game has radically expanded, adding extra maps, new operators, weapons, and themed seasonal events.',
+        'trailer': 'https://www.youtube.com/watch?v=6wlvYh0h63k',
+        'count_in_stock': 15,
+        'price': '10'
+    },
+
+
+
+]
+
+
+export default games

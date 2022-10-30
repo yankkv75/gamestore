799f9873673f38f8d5e3429a4756b51c15c1fec5
Vadym Yankovskyi <yankkv75@gmail.com>
2022-10-30 21:15:17 +0300

    fetching data from backend

--- frontend/src/components/BlogItem.js
+++ frontend/src/components/BlogItem.js
@@ -3,9 +3,7 @@ import { Link } from 'react-router-dom'
 import '../static/css/Blog.css'
 
 function BlogItem({ post }) {
-
     return (
-
         <div className='cards-item'>
             <Link to={`post/${post._id}`} className='cards-item-link'>
                 <figure className='cards-item-pic-wrap'>
@@ -23,8 +21,6 @@ function BlogItem({ post }) {
                 </div>
             </Link>
         </div>
-
-
     )
 }
 

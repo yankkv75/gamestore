4fd60ea3b470b6eff5516245d55df408d9ff2672
Vadym Yankovskyi <yankkv75@gmail.com>
2022-10-30 21:18:38 +0300

    fetching data from backend

--- frontend/src/screens/GamesScreen.js
+++ frontend/src/screens/GamesScreen.js
@@ -1,8 +1,21 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
 import GameItem from '../components/GameItem'
-import games from '../games'
 import '../static/css/Games.css'
 
 function GamesScreen({ }) {
+
+    const [games, setGames] = useState([])
+
+    useEffect(() => {
+        async function fetchGames() {
+            const { data } = await axios.get('api/games/')
+            setGames(data)
+        }
+        fetchGames()
+    }, [])
+
     return (
         <div className='games-container'>
             <p className='games-top'>Our Games</p>

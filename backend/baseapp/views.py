08ee62d2a6394f0205eab9bce894713edb2bcef4
Vadym Yankovskyi <yankkv75@gmail.com>
2022-10-31 21:24:29 +0300

    feat: change views for real data

--- backend/baseapp/views.py
+++ backend/baseapp/views.py
@@ -1,57 +1,34 @@
-import imp
-import pkgutil
-from django.http import JsonResponse
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .posts import posts
-from .games import games
+from .models import Post, Game
+from .serializers import GameSerializer, PostSerializer
 # Create your views here.
 
 
 @api_view(['GET'])
-def get_routes(request):
-    routes = [
-        '/api/posts/',
-        '/api/posts/create/',
-
-        '/api/posts/upload/',
-
-        '/api/posts/<id>/reviews/',
-
-        '/api/posts/top/',
-        '/api/posts/<id>',
-
-        '/api/posts/delete/<id>',
-        '/api/posts/<update>/<id>',
-    ]
-    return Response(routes)
-
-
-@api_view(['GET'])
 def get_posts(request):
-    return Response(posts)
+    posts = Post.objects.all()  # return all posts from db
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
+
 
 @api_view(['GET'])
 def get_post(request, pk):
-    post = None
-    for i in posts:
-        if i['_id'] == pk:
-            post = i
-            break
-    return Response(post)
+    post = Post.objects.get(_id=pk)
+    serializer = PostSerializer(post, many=False)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 def get_games(request):
-    return Response(games)
+    games = Game.objects.all()
+    serializer = GameSerializer(games, many=True)
+    return Response(serializer.data)
+
 
 @api_view(['GET'])
 def get_game(request, pk):
-    game = None
-    for i in games:
-        while i['_id'] == pk:
-            game = i
-            break
-    return Response(game)
+    game = Game.objects.get(_id=pk)
+    serializer = GameSerializer(game, many=False)
+    return Response(serializer.data)

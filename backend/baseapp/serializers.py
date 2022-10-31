ae9cebba9f746d0c2f3899e995354d1d38068b25
Vadym Yankovskyi <yankkv75@gmail.com>
2022-10-31 21:21:33 +0300

    feat: add serializers for posts and games

---
+++ backend/baseapp/serializers.py
@@ -0,0 +1,20 @@
+from dataclasses import field
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Post, Game
+
+# They going to wrap model and then it's going to turn that into JSON
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        # 1. model, that need to be serialized
+        model = Post
+        # 2. field, that we want to render out
+        fields = '__all__'
+
+
+class GameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = '__all__'

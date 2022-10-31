05c8f9aad06d29a3032363a4be53687773bbdbd9
Vadym Yankovskyi <yankkv75@gmail.com>
2022-10-31 21:26:08 +0300

    fix: removed unnecessary path

--- backend/baseapp/urls.py
+++ backend/baseapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_routes, name='routes'),
     path('posts/', views.get_posts, name='posts'),
     path('post/<str:pk>/', views.get_post, name='post'),
 

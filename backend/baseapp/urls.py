d0992ea6782c92defe8e190885cf7ac102d7e705
Vadym Yankovskyi <yankkv75@gmail.com>
2022-11-14 00:24:51 +0300

    feat: creating user auth urls

--- backend/baseapp/urls.py
+++ backend/baseapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
+    path('users/login/', views.MyTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
     path('posts/', views.get_posts, name='posts'),
     path('post/<str:pk>/', views.get_post, name='post'),
-
     path('games/', views.get_games, name='games'),
     path('game/<str:pk>/', views.get_game, name='game'),
+    path('users/profile/', views.get_user, name='user'),
 ]

8e8d8465a7a78a2d59d1f05d862d8d9edbecdfd9
Vadym Yankovskyi <yankkv75@gmail.com>
2022-11-14 00:26:32 +0300

    feat: implementing jwt token serializer and view

--- backend/baseapp/views.py
+++ backend/baseapp/views.py
@@ -2,10 +2,30 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import Post, Game
-from .serializers import GameSerializer, PostSerializer
-# Create your views here.
+from .serializers import GameSerializer, PostSerializer, UserSerializer, UserSerializerWithToken
+
+# Customizing jwt token
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # return back user information
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+# Post views
 @api_view(['GET'])
 def get_posts(request):
     posts = Post.objects.all()  # return all posts from db
@@ -20,6 +40,7 @@ def get_post(request, pk):
     return Response(serializer.data)
 
 
+# Games views
 @api_view(['GET'])
 def get_games(request):
     games = Game.objects.all()
@@ -31,4 +52,21 @@ def get_games(request):
 def get_game(request, pk):
     game = Game.objects.get(_id=pk)
     serializer = GameSerializer(game, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_posts(request):
+    posts = Post.objects.all()  # return all posts from db
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
+
+
+# User views
+
+# gets the token
+@api_view(['GET'])
+def get_user(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
     return Response(serializer.data)

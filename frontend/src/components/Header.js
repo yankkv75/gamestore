e7ae140bc908c003c64d03dd05aa3de7edbeda1a
Vadym Yankovskyi <yankkv75@gmail.com>
2022-11-17 01:03:12 +0300

    feat: update header, add logout functionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link as LinkRouter } from 'react-router-dom';
 import { Link as LinkScroll } from 'react-scroll'
 import { animateScroll as scroll } from 'react-scroll'
 import '../static/css/Header.css'
-
+import { logout } from '../actions/userActions'
 
 function Header() {
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+    const dispatch = useDispatch()
+
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
 
     const [click, setClick] = useState(false)
 
@@ -33,16 +42,28 @@ function Header() {
                     <li className='nav-item'>
                         <LinkScroll className='nav-link' to='services' smooth={true} duration={700} spy={true} exact='true' offset={-80} onClick={closeMobileMenu}>Services</LinkScroll>
                     </li >
-                    <li className='nav-item'>
-                        <LinkScroll className='nav-link' to='sign-up' smooth={true} duration={700} spy={true} exact='true' offset={-80} onClick={closeMobileMenu}>Sign Up</LinkScroll>
-                    </li >
 
+                    {userInfo ? (
+                        <li className='nav-item'>
+                            <LinkRouter to='/profile' className='nav-link'>
+                                {userInfo.username}
+                            </LinkRouter>
+                        </li >
+                    ) : (
+                        <li className='nav-item'>
+                            <LinkScroll className='nav-link' to='sign-up' smooth={true} duration={700} spy={true} exact='true' offset={-80} onClick={closeMobileMenu}>Sign Up</LinkScroll>
+                        </li >
+                    )}
                 </ul>
-
-                <button className='nav-btn'>
-                    <LinkRouter to='/sign-in' className='nav-btn-link'>Sign In</LinkRouter>
-                </button>
-
+                {userInfo ? (
+                    <button onClick={logoutHandler} className='nav-btn'>
+                        <div className='nav-btn-link'>Log Out</div>
+                    </button>
+                ) : (
+                    <button className='nav-btn'>
+                        <LinkRouter to='/sign-in' className='nav-btn-link'>Sign In</LinkRouter>
+                    </button>
+                )}
             </div>
         </div>
     )

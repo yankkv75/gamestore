81e64079835fd739805b417f66c06a27469e48d7
Vadym Yankovskyi <yankkv75@gmail.com>
2022-11-16 23:46:14 +0300

    feat: create user constants, reducer, action     * update store for pulling data from the local storage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,6 +12,9 @@ import {
 import {
     cartReducer
 } from './reducers/cartReducers'
+import {
+    userLoginReducer
+} from './reducers/userReducers'
 
 const reducer = combineReducers({
     postList: postListReducer,
@@ -19,14 +22,21 @@ const reducer = combineReducers({
     gameList: gameListReducer,
     gameDetails: gameDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
 })
 
 // Pull data from the local storage, parse it to turn back into a js object and load it into initial state
 const cartItemsLocalStorage = localStorage.getItem('cartItems') ?
     JSON.parse(localStorage.getItem('cartItems')) : []
 
+const userInfoLocalStorage = localStorage.getItem('userInfo') ?
+    JSON.parse(localStorage.getItem('userInfo')) : []
+
+
+
 const initalState = {
-    cart: { cartItems: cartItemsLocalStorage }
+    cart: { cartItems: cartItemsLocalStorage },
+    userLogin: { userInfo: userInfoLocalStorage }
 }
 
 const middleware = [thunk]
